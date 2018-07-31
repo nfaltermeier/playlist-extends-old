df817d21417397d3bfdfcd24d1f14691fb11e82a
Pyrrhic <nefalt2@gmail.com>
2018-07-31 00:23:14 -0500

    Declare types, init TS project

---
+++ src/Types.d.ts
@@ -0,0 +1,24 @@
+export interface IParser {
+  /**
+   * Returns true if the parser will accept the file based on file name
+   *
+   * @param fileName the name of the file to check whether to parse or not
+   */
+  canParseString(fileName: string): boolean;
+
+  parseFile(fileContents: string, fileName: string): null;
+
+  createFile(): string;
+}
+
+export class Song {
+  title: string;
+  artist: string;
+  filePath: string;
+  spotifyID: string;
+}
+
+export class Playlist {
+  title: string;
+  songs: Song[];
+}
